651189854f83360e72f73ab3db0d6fe960e21ec0
Paolo Bianchini <paolob_67@yahoo.com>
2020-05-29 12:45:48 +0200

    reformat APP Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,4 @@
 /**
- * App Routing module 
- * Application cromponent defines routes for 
- * tutorial, acocunt, support, login, signup
- * and app.
- * App routes are ahndled by the tabs page routing module and 
- * appear in the upper part of the menu
  * @author Paolo Bianchini
  * @author Lorenzo Monaco
  */
@@ -22,45 +16,20 @@ import {
 import {
   CheckTutorial
 } from './providers/check-tutorial.service';
+import {
+  APP_ROUTES
+} from './app.routes';
 
-/** 
- * JSON object defining routes 
+/**
+ * App Routing module 
+ * Application cromponent defines routes for 
+ * tutorial, acocunt, support, login, signup
+ * and app.
+ * App routes are ahndled by the tabs page routing module and 
+ * appear in the upper part of the menu
  */
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/tutorial',
-    pathMatch: 'full'
-  },
-  {
-    path: 'account',
-    loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
-  },
-  {
-    path: 'support',
-    loadChildren: () => import('./pages/support/support.module').then(m => m.SupportModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignUpModule)
-  },
-  {
-    path: 'app',
-    loadChildren: () => import('./pages/tabs-page/tabs-page.module').then(m => m.TabsModule)
-  },
-  {
-    path: 'tutorial',
-    loadChildren: () => import('./pages/tutorial/tutorial.module').then(m => m.TutorialModule),
-    canLoad: [CheckTutorial]
-  }
-];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,49 @@
+/**
+ * App Routes
+ * JSON object defining routes for application
+ * @author Paolo Bianchini
+ * @author Lorenzo Monaco
+ */
+
+/**
+ * Imports
+ */
+import {
+  Routes
+} from '@angular/router';
+import {
+  CheckTutorial
+} from './providers/check-tutorial.service';
+
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    redirectTo: '/tutorial',
+    pathMatch: 'full'
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
+  },
+  {
+    path: 'support',
+    loadChildren: () => import('./pages/support/support.module').then(m => m.SupportModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignUpModule)
+  },
+  {
+    path: 'app',
+    loadChildren: () => import('./pages/tabs-page/tabs-page.module').then(m => m.TabsModule)
+  },
+  {
+    path: 'tutorial',
+    loadChildren: () => import('./pages/tutorial/tutorial.module').then(m => m.TutorialModule),
+    canLoad: [CheckTutorial]
+  }
+];
