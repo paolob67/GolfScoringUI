9f67a7a58c1a19e176a2d056eaf85843513a05c9
Paolo Bianchini <paolob_67@yahoo.com>
2020-06-01 15:46:12 +0200

    Comments

--- src/app/pages/leaderboard-mob/leaderboard-mob.ts
+++ src/app/pages/leaderboard-mob/leaderboard-mob.ts
@@ -1,3 +1,11 @@
+/**
+ * @author Paolo Bianchini
+ * @author Lorenzo Monaco
+ */
+
+/**
+ * Imports
+ */
 import {
   Component
 } from '@angular/core';
@@ -11,7 +19,6 @@ import {
 import {
   RestClientService
 } from '../../providers/rest-client.service';
-
 import {
   CoursesResponse,
   EventsResponse,
@@ -23,23 +30,52 @@ import {
   DetailedLeaderboardResponse
 } from '../../interfaces/rest-datamodel';
 
-
-
+/**
+ * This component shows the leaderboard for the event that was selected 
+ * in the previuos page. The page is divided in several sections.
+ * The totals and the rounds. On the totals section we display cumulative
+ * scores for all the rounds. In the rounds sections we display detailed
+ * leaderboard data for all the players competing. If the user clicks
+ * on the player list in the totals section the app displays detailed 
+ * score info for the selected player.
+ */
 @Component({
   selector: 'page-leaderboard-mob',
   templateUrl: 'leaderboard-mob.html',
   styleUrls: ['./leaderboard-mob.scss'],
 })
 export class LeaderboardMobPage {
+  /**
+   * Holds cumulative scores for the event
+   */
   leaderboardTotal: any[] = [];
+  /**
+   * the selected event id
+   */
   eventId = '';
+  /**
+   * Event info
+   */
   event: EventsResponse;
+  /**
+   * Scores for event
+   */
   scores: RoundScoresResponse[] = [];
+  /** 
+   * passed on the route if 0 go to totals tab
+   */
   showRound = '0'; // passed on the route if 0 go to totals tab
-
+  /**
+   * Select mob vire rendering
+   */
   mobview = false;
+  /**
+   * holds number of rounds for event. init to 1
+   */
   numberOfRounds = 1;
-
+  /**
+   * segment to show. init to totals
+   */
   segment = 'Totals';
 
   constructor(
@@ -48,29 +84,40 @@ export class LeaderboardMobPage {
     public userData: UserData,
     public restClient: RestClientService
   ) {
+    // load properties from url
     this.eventId = this.route.snapshot.paramMap.get('eventId');
     this.showRound = this.route.snapshot.paramMap.get('showRound');
+    // if the passed round is 0 then show the totals tab
     if (this.showRound !== '0') {
       this.segment = 'Round ' + this.showRound;
     }
   }
 
+  /**
+   * When entering the view load leaderboard data from server.
+   * This needs to bperfromed every time the user enters the view
+   * because data could change in the meantime
+   * by calling [getLeaderboardDetails]{@link RestClientService#getLeaderboardDetails}
+   * TODO: add timeout to refresh data.
+   */
   ionViewDidEnter() {
     this.restClient.presentLoader();
+    // first load event data
     this.restClient.getEvent(this.eventId)
       .subscribe(
         (response: EventsResponse) => {
+          // save envet info
           this.event = response;
           this.eventId = response.id;
-          console.log('Event Id: ' + this.eventId);
           this.numberOfRounds = response.numberOfRounds;
+          // get leaderboard data for totals
           this.restClient.getLeaderboardDetails(this.eventId)
             .subscribe(
               (responsesco: DetailedLeaderboardResponse[]) => {
                 this.leaderboardTotal = responsesco;
-                console.log('Scores L: ' + JSON.stringify(this.leaderboardTotal));
-                let rnd;
-                for (rnd = 1; rnd <= this.numberOfRounds; rnd++) {
+                //console.log('Scores L: ' + JSON.stringify(this.leaderboardTotal));
+                // load data for each round of the event
+                for (let rnd = 1; rnd <= this.numberOfRounds; rnd++) {
                   this.loadLeaderboard(rnd);
                 }
               },
@@ -91,8 +138,13 @@ export class LeaderboardMobPage {
       );
   }
 
+  /**
+   * Load leaderboard data fro each round of the event
+   * by calling [getRoundScoresDetails]{@link RestClientService#getRoundScoresDetails}
+   * @param round the round for the event
+   */
   loadLeaderboard(round) {
-    // get Detailed Scores
+    // get detailed Scores
     this.restClient.presentLoader();
     this.restClient.getRoundScoresDetails(this.eventId, round)
       .subscribe(
@@ -102,25 +154,13 @@ export class LeaderboardMobPage {
         },
         err => {
           console.log('Error getting Round of Leaderboard', err.error.error);
+          this.restClient.dismissLoader();
         },
         () => {
           this.restClient.dismissLoader();
         }
       );
 
-  }
-
-  getColor(score: number) {
-    const colors = [
-      'primary',
-      'secondary',
-      'tertiary',
-      'success',
-      'warning',
-      'danger'
-    ];
-
-    return colors[score - 1];
   }
 
 }
