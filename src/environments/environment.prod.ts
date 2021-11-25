9f956eb402a63b341815e0f791fc57c8d155ef5e
Paolo Bianchini <5672449+paolob67@users.noreply.github.com>
2021-11-25 19:31:50 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,5 @@
 
 export const environment = {
   production: true,
-  apiEndPoint: 'http://golfscore.rackone.io:3000'
+  apiEndPoint: 'http://golf-scoring-apiserver-pb-golfscoring.itzroks-50e8v73tt8-p4oocr-6ccd7f378ae819553d37d5f2ee142bd6-0000.eu-gb.containers.appdomain.cloud'
 };
